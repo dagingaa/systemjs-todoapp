3526156387b0fdb0ad000a6526e128c70c8c050b
Dag-Inge Aas <dagingaa@comoyo.com>
2015-01-11 02:05:36 +0100

    Add better View and Model handling
    This change adds better View and Model handling.
    
    First off, we introduce the `Observable` class, which exposes on function
    `.on(eventName, callback)` which fires if the Object itself has changes.
    This in and of itself seems like a good idea, but after extensive
    testing, isn't a so good idea. Some changes are needed.
    
    Second, we introduce the `View` class, which exposes a `render` and
    `remove` function common and overridable for all `View`s. The meaning of
    this is to create a common API for View stuff so we can be sure that the
    element always is created etc. No error handling is present for this, so
    if you pass in an empty element, well then, shit happens. Needs work.
    
    All in all, I'm quite pleased with having touched Object.observe,
    template string and EventEmitter in one pull request. Hopefully more to
    come.

--- config.js
+++ config.js
@@ -1,7 +1,35 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "todo/*": "lib/*.js"
+    "todo/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "observed": "npm:observed@1.1.0",
+    "github:jspm/nodelibs-events@0.1.0": {
+      "events-browserify": "npm:events-browserify@0.0.1"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "github:jspm/nodelibs-tty@0.1.0": {
+      "tty-browserify": "npm:tty-browserify@0.0.0"
+    },
+    "npm:debug@0.7.4": {
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "tty": "github:jspm/nodelibs-tty@0.1.0"
+    },
+    "npm:events-browserify@0.0.1": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    },
+    "npm:observed@1.1.0": {
+      "debug": "npm:debug@0.7.4",
+      "events": "github:jspm/nodelibs-events@0.1.0"
+    }
   }
 });
 
